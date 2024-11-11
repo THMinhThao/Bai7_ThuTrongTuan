82af4d20289d6ed85d25aacc8f57bbe999edc69c
THMinhThao <thaominh245@gmail.com>
2024-11-11 16:11:36 +0700

    Add project files.

---
+++ Bai7_ThuTrongTuan/Bai7_ThuTrongTuan.cpp
@@ -0,0 +1,41 @@
+// Bai7_ThuTrongTuan.cpp : 
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int thu;
+    cout << "Nhap Thu";
+    cin >> thu; 
+    switch (thu)
+
+
+
+    {
+    case 2:
+        cout << "thu hai\n";
+        break;
+    case 3:
+        cout << "thu ba\n";
+        break;
+    case 4:
+        cout << "thu bon\n";
+        break;
+    case 5:
+        cout << "thu nam\n";
+        break;
+    case 6:
+        cout << "thu sau\n";
+        break;
+    case 7:
+        cout << "thu bay\n";
+        break;
+    case 8:
+        cout << "chu nhat\n";
+        break;
+    }
+      return 0;
+}
+
+
